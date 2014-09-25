862ba9e7cadfcb5f35c4b52e6a1d8b0f5375dd7c
Claudio Mulas <claudio.mulas@lucla.biz>
2014-09-26 01:30:07 +0200

    Update result.php
    Test 1 done

--- php/result.php
+++ php/result.php
@@ -3,8 +3,8 @@
 # Our Current Location
 $location = ['51.301819','-0.337613'];
 
-$link = mysqli_connect("localhost","root","","postboxes");
-$result = $link->query("SELECT * FROM boxes WHERE lat LIKE '%51%' AND lng LIKE '%-0.33%' LIMIT 10");
+$link = mysqli_connect("localhost","root","luchi1","postboxes");
+$result = $link->query("SELECT *,(((acos(sin((".$location[0]."*pi()/180)) * sin((`lat`*pi()/180))+cos((".$location[0]."*pi()/180)) * cos((`lat`*pi()/180)) * cos(((".$location[1]."- `lng`)*pi()/180))))*180/pi())*60*1.1515) as distance FROM boxes ORDER BY distance LIMIT 10");
 
 $output = "<table>";
 while($row = mysqli_fetch_array($result)) {
@@ -15,6 +15,7 @@ while($row = mysqli_fetch_array($result)) {
     $output .= "<td>".$row['depot']."</td>";
     $output .= "<td>".$row['outcode']."</td>";
     $output .= "<td>Collected At: ".$row['collection']."</td>";
+    $output .= "<td>".number_format($row['distance'],3)." miles</td>";
   $output .= "</tr>";
   
 }
