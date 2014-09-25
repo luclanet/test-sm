0f91a545bd24c639aabd63cac6cff62dcc1e6145
Claudio Mulas <claudio.mulas@lucla.biz>
2014-09-26 01:32:17 +0200

    Update script.js
    Test 3 done

--- javascript/one/script.js
+++ javascript/one/script.js
@@ -1,3 +1,22 @@
 (function(){
-	// Page code in here.
+	document.getElementsByTagName("form")[0].addEventListener("submit", function(e){
+		if (document.getElementById("text").value.length == 0) {
+			alert("String is empty");
+			e.preventDefault();
+		}
+		else if (document.getElementById("text").value.length > 255) {
+			alert("String is longer of 255 characters");
+			e.preventDefault();
+		}
+		else if (document.getElementById("text").value.indexOf("palindrome") !== -1) {
+			alert("There is the word 'palindrome'");
+			e.preventDefault();
+		}
+		else {
+			if (document.getElementById("text").value == document.getElementById("text").value.split('').reverse().join('')) {
+				alert("Aaaahhhrrrggg! A palindrome! I hate them!");
+				e.preventDefault();
+			}
+		}
+	});
 })();
